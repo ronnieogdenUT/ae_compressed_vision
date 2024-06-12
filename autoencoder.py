453d7d55b5957eae6b9c70b491785613e9014ce9
Gaurav Mitra <mitragaurav10@gmail.com>
2024-06-12 13:18:33 -0500

    Fixed padding sizes for encoder + decoder

--- autoencoder.py
+++ autoencoder.py
@@ -37,10 +37,6 @@ device = (
 )
 print(f"Using {device} device")
 
-#Calculate Padding Size
-#padding = 2*(64 - 1) - 64 + 2  = 64
-#padding = Stride * (input Vol Size - 1) - input Vol Size + kernel size
-
 class resblock_a(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -105,27 +101,29 @@ class Autoencoder(torch.nn.Module):
         
         # Figure out if resblock_c needs to be a transposed version... I think they are the same here		
         #Decoder
-        self.decoderConv1 = torch.nn.ConvTranspose3d(32, 128, 3, stride=(1,2,2), padding= 0)
+        self.decoderConv1 = torch.nn.ConvTranspose3d(32, 128, 3, stride=(1,2,2), padding=1, output_padding=(0,1,1))
         self.decoderBn1 = torch.nn.BatchNorm3d(128)
-        self.decoderConv2 = torch.nn.ConvTranspose3d(128, 64, 5, stride=(1,2,2), padding= 0)
+        self.decoderConv2 = torch.nn.ConvTranspose3d(128, 64, 5, stride=(1,2,2), padding=2, output_padding=(0,1,1))
         self.decoderBn2 = torch.nn.BatchNorm3d(64)
-        self.decoderConv3 = torch.nn.ConvTranspose3d(64, in_channels, 5, stride=(1,2,2), padding= 0)
+        self.decoderConv3 = torch.nn.ConvTranspose3d(64, in_channels, 5, stride=(1,2,2), padding=2, output_padding=(0,1,1))
         self.decoderBn3 = torch.nn.BatchNorm3d(in_channels)
 
     def forward(self, x):
         stride = (1,2,2)
+
         #Encoder
-        print(self.calc_pad(x, stride, 5))
-        f.pad(x, (2,1,1,2,1,2))
+        x = f.pad(x, self.same_pad(x, stride, 5))
         x = self.encoderConv1(x)
         x = self.encoderBn1(x)
         x = f.relu(x)
         
+        x = f.pad(x, self.same_pad(x, stride, 5))
         x = self.encoderConv2(x)
         x = self.encoderBn2(x)
         x = f.relu(x)
         resblock_c()
 
+        x = f.pad(x, self.same_pad(x, stride, 5))
         x = self.encoderConv3(x)
         x = self.encoderBn3(x)
 
@@ -145,34 +143,45 @@ class Autoencoder(torch.nn.Module):
         return x
     
     #Calculates Padding(Mimics Tensor Flow padding = 'same')
-    def calc_pad(self, x, stride, kernel):
+    def same_pad(self, x, stride, kernel):
         size = x.size()
-        print (size)
-        in_height = size[6]
-        in_width = size[5]
-        filter_height = kernel[0]
-        filter_width = kernel[1]
+        in_height = size[4]
+        in_width = size[3]
+        filter_height = kernel
+        filter_width = kernel
 
         out_height = math.ceil(float(in_height) / float(stride[1]))
         out_width  = math.ceil(float(in_width) / float(stride[2]))
 
-        #3D Padding: Time Dimension
+        #3D Padding: Time Dimension, Front, Back
+        pad_forward = (kernel-1)//2
+        pad_backward = (kernel-1)//2
+
+        
+        #Regular 2D Padding: Top, Bottom, Left, Right
         pad_along_height = max((out_height - 1) * stride[1] + filter_height - in_height, 0)
         pad_along_width = max((out_width - 1) * stride[2] + filter_width - in_width, 0)
-
-        #Regular 2D Padding: Top, Bottom, Left, Right
         pad_top = pad_along_height // 2
         pad_bottom = pad_along_height - pad_top
         pad_left = pad_along_width // 2
         pad_right = pad_along_width - pad_left
+        output = (pad_left, pad_right, pad_top, pad_bottom, pad_forward, pad_backward)
+        return output
+
+    def same_pad_decoder(self, x, stride, kernel):
+        size = x.size()
+        print(size)
+        output = (0,0,0,0,0,0)
+        return output
     
 #Training Method with MSE Loss Function and Adam Optimizer
 def train(dataloader, model, loss_fn, optimizer):
     output = []
     size = len(dataloader.dataset)
     model.train()
-
+    batch_num = 1
     for batch in dataloader:
+        print ("Batch: " + str(batch_num))
         video = batch.to(torch.float32)
         video = torch.permute(video, (0,2,1,3,4))
         # Output of Autoencoder
@@ -190,6 +199,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
         # Storing the losses in a list for plotting
         losses.append(loss.item())
+        batch_num = batch_num + 1
     return reconstructed
 
 
@@ -250,7 +260,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr = 0.01, betas=(0.9,0.999))
 
 epochs = 3
 losses = []
+epoch = 1
 for epoch in range(epochs):
+    epoch = 1
     print ("Epoch: " + str(epoch))
     train(train_loader, model, loss_fn, optimizer)
 """
