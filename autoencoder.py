9f25bc9ff22bb69f4eaa70f30b72fa8d912b23f4
Pranad Muttoju <pm32764@my.utexas.edu>
2024-07-09 14:29:49 -0500

    Train

--- autoencoder.py
+++ autoencoder.py
@@ -417,6 +417,6 @@ def main(is_train, model_name, codebook_length):
 #Call Main Function
 model_name = "modelQuant.pth"
 model_exist = True
-is_train = False
+is_train = True
 codebook_length = 20
 main(is_train, model_name, codebook_length)
