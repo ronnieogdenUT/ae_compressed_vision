892946b03f7632ca517b74ad81bda004ab2e415f
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-18 13:56:47 -0500

    Fix 7

--- rate_distortion.py
+++ rate_distortion.py
@@ -19,14 +19,17 @@ from train import train
 def rate_distortion(train_loader, model_name, codebook_length, device, model_exist):
     codebook_vals = [10, 30, 50, 100, 200]
     is_show = False
-    last_loss = sys.maxsize
-    curr_loss = 0
+    last_loss = 10
+    curr_loss = 5
     first = True
     losses = []
     for codebook_length in codebook_vals:
         while (abs(last_loss-curr_loss)/last_loss < 0.01):
             if (first):
                 model_name = model_name + str(codebook_length) + '.pth'
+                curr_loss = train(train_loader, model_name, codebook_length, device, model_exist, is_show)
+                last_loss = curr_loss
+                continue
             else:
                 model_exist = True
                 last_loss = curr_loss
