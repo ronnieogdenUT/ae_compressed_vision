38b0b7ed41b09df4cdd3704e25fb4eda5e8daf03
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-21 01:17:10 -0500

    Fix 1

--- rate_distortion.py
+++ rate_distortion.py
@@ -8,7 +8,6 @@ from datetime import timedelta
 
 def rate_distortion(train_loader, test_loader, model_name, codebook_length, device):
     codebook_vals = [64, 128, 256, 512, 1024]
-    os.system("nvidia-smi")
     is_show = False
     epochs = 1
     for codebook_length in codebook_vals:
