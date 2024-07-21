246fa48f924b749fb7d19124d469b3925e701b43
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-21 01:25:07 -0500

    Fix 2

--- rate_distortion.py
+++ rate_distortion.py
@@ -7,7 +7,7 @@ import datetime
 from datetime import timedelta
 
 def rate_distortion(train_loader, test_loader, model_name, codebook_length, device):
-    codebook_vals = [1024]
+    codebook_vals = [512]
     is_show = False
     epochs = 1
     for codebook_length in codebook_vals:
