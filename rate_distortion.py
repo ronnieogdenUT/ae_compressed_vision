7b9f6d04fe9ce6e1905ea8ea7922157725c820d3
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-22 15:20:54 -0500

    Fix 6

--- rate_distortion.py
+++ rate_distortion.py
@@ -19,8 +19,6 @@ def train_rate_distortion(train_loader, test_loader, model_name, codebook_length
         model_codename = model_name + str(codebook_length)
         overfit = False
         train(train_loader, model_codename, codebook_length, device, model_exist, is_show, epochs, batch_size)
-        train(train_loader, model_codename, codebook_length, device, model_exist, is_show, epochs, batch_size)
-        train(train_loader, model_codename, codebook_length, device, model_exist, is_show, epochs, batch_size)
         while (abs(last_loss-curr_loss)/last_loss > 0.01 and overfit == False):
             start = time.perf_counter()
             if (first):
