54557e66c4546d775ee5a94db043a9613999f3fe
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-25 13:24:54 -0500

    Fix 10

--- videoprocessor.py
+++ videoprocessor.py
@@ -15,7 +15,9 @@ data = datasets.MovingMNIST(
 )
 # 10,000 x 20 x 1 x 64 x 64
 video = data[0] # 1 x 20 x 1 x 64 x 64
+print (video.shape)
 video = torch.permute(video, (1,0,2,3,4)) #Frames x B x C x L x W
+print (video.shape)
 batch_size = 2
 
 in_channels = 1
