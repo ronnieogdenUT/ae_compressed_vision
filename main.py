017e3733f25dc57f0b09c2d5b2b11d2b2a0212f4
Pranad Muttoju <pm32764@my.utexas.edu>
2024-07-30 15:26:07 -0500

    main.py updated

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ device = (
 print(f"Using {device} device")
 
 #Call Main Function
-model_exist = True
+model_exist = False
 codebook_length = 20
 function_run = sys.argv[1]
 model_name = sys.argv[2] + ".pth"
