53afa0b1e74aa1689d44e428849a7feb54030538
Gaurav Mitra <mitragaurav10@gmail.com>
2024-07-31 15:18:16 -0500

    Fix 4

--- videoprocessor.py
+++ videoprocessor.py
@@ -47,7 +47,6 @@ for video in train_loader:
         frameSet = torch.permute(video, (1,0,2,3,4)) #Change to Frames x 1 x C x L x W
         frameSet = frameSet[0:2, :] #Cut it to 2 x 1 x C x L x W
         frameSet = torch.permute(frameSet, (1,2,0,3,4)) #Permute it to B x C x 2 x 64 x 64
-        print(frameSet.shape)
         frameSet = frameSet.to(device)
         frameSet = frameSet.to(torch.float32)
         start = time.perf_counter()
