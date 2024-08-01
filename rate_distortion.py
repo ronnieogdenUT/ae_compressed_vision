fc35b458c40c99ca7cae87a4719e865bbaa3a87e
Gaurav Mitra <mitragaurav10@gmail.com>
2024-08-01 14:43:28 -0500

    Fix 5

--- rate_distortion.py
+++ rate_distortion.py
@@ -21,7 +21,7 @@ def train_rate_distortion(train_loader, test_loader, model_name, codebook_length
     i = 1
     initial_loops = 20
 
-    while ((abs(last_loss-curr_loss)/last_loss > 0.1 and overfit == False) or initial_loops):
+    while ((abs(last_loss-curr_loss)/last_loss > 0.1 and overfit == False) or i <= initial_loops):
         start = time.perf_counter()
         if (first):
             files = os.scandir('models')
