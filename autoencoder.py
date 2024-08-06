15604f6a5d3932385d98a1cfc7795da4164ce44b
Gaurav Mitra <mitragaurav10@gmail.com>
2024-08-05 19:31:57 -0500

    Debug 8

--- autoencoder.py
+++ autoencoder.py
@@ -188,7 +188,7 @@ class Autoencoder(torch.nn.Module):
 
         #Qs = torch.permute(Qs, (1,2,3,4,0,5))
         #print(Qs.shape)
-        quantized_x = (Qs)/torch.sum(Qs)
+        quantized_x = torch.div(Qs, torch.sum(Qs))
 
         #Multiply Qs with centroids to get closest Codebook Value
         #Multiplies Qs(L x 16 x 32 x 20 x 8 x 8) and centroids(L x 16 x 32 x 20 x 8 x 8) and converts to tensor
