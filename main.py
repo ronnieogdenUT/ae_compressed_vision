e2dcaa42b8d671decec41bcdbf9663f149712ece
Gaurav Mitra <mitragaurav10@gmail.com>
2024-09-30 08:53:17 -0500

    Debug 2

--- main.py
+++ main.py
@@ -60,63 +60,63 @@ for file in files:
 batch_size = 16 #1
 curr_ind = 0
 losses = []
-# while True:
-#Initialize Dataloader over training data
-train_loader = torch.utils.data.DataLoader(
-    dataset = data,
-    batch_size = batch_size, 
-    sampler = train_sampler,
-    num_workers = 4,
-    pin_memory = True
-)
+while True:
+    #Initialize Dataloader over training data
+    train_loader = torch.utils.data.DataLoader(
+        dataset = data,
+        batch_size = batch_size, 
+        sampler = train_sampler,
+        num_workers = 4,
+        pin_memory = True
+    )
 
-#Initialize Dataloader over test data
-test_loader = torch.utils.data.DataLoader(
-    dataset = data,
-    batch_size = batch_size,
-    sampler = test_sampler
-)
-    # try:    
-if function_run == 'train':
-    is_show = False
-    epochs = int(sys.argv[3])
-    train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
-elif function_run == 'testTrain':
-    is_show = True
-    epochs = 1
-    train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
-elif function_run == 'showtrain':
-    is_show = True
-    epochs = int(sys.argv[3])
-    train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
-elif function_run == 'test':
-    is_show = True
-    test(test_loader, model_name, codebook_length, device, is_show, batch_size)
-elif function_run == 'train-rate-distortion':
-    codebook_vals = [2, 4, 8, 16, 32, 64,128]
-    train_rate_distortion(train_loader, test_loader, model_name, codebook_vals[curr_ind], device, batch_size)
-    if (curr_ind + 1 != len(codebook_vals)): 
-        curr_ind += 1
-        #continue
-elif function_run == 'show-rate-distortion':
-    codebook_vals = [2, 4, 8, 16, 32, 64, 128]
-    loss = show_rate_distortion(test_loader, model_name, codebook_vals[curr_ind], device, batch_size)
-    losses.append(loss)
-    if (curr_ind + 1 != len(codebook_vals)): 
-        curr_ind += 1
-        #continue
-    else:
-        plt.plot(codebook_vals, losses)
-        plt.xlabel('Codebook Values')
-        plt.ylabel('Loss')
-        plt.title('Rate-Distortion')
-        plt.show()
-else:
-    print("Unknown Function")
-    print("Usage: python3 main.py [function] [model] [epochs]")
-    print("Functions: train, testTrain, showtrain, test, train-rate-distortion, show-rate-distortion")
-    # except RuntimeError:
-    #     print("CUDA Out of Memory. Decreasing Batch Size by Half. New Batch Size: " + str(batch_size/2))
-    #     batch_size = int(batch_size/2)
-    #     continue
-    # break
+    #Initialize Dataloader over test data
+    test_loader = torch.utils.data.DataLoader(
+        dataset = data,
+        batch_size = batch_size,
+        sampler = test_sampler
+    )
+    try:    
+        if function_run == 'train':
+            is_show = False
+            epochs = int(sys.argv[3])
+            train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
+        elif function_run == 'testTrain':
+            is_show = True
+            epochs = 1
+            train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
+        elif function_run == 'showtrain':
+            is_show = True
+            epochs = int(sys.argv[3])
+            train(train_loader, model_name, codebook_length, device, model_exist, is_show, epochs, batch_size)
+        elif function_run == 'test':
+            is_show = True
+            test(test_loader, model_name, codebook_length, device, is_show, batch_size)
+        elif function_run == 'train-rate-distortion':
+            codebook_vals = [2, 4, 8, 16, 32, 64,128]
+            train_rate_distortion(train_loader, test_loader, model_name, codebook_vals[curr_ind], device, batch_size)
+            if (curr_ind + 1 != len(codebook_vals)): 
+                curr_ind += 1
+                continue
+        elif function_run == 'show-rate-distortion':
+            codebook_vals = [2, 4, 8, 16, 32, 64, 128]
+            loss = show_rate_distortion(test_loader, model_name, codebook_vals[curr_ind], device, batch_size)
+            losses.append(loss)
+            if (curr_ind + 1 != len(codebook_vals)): 
+                curr_ind += 1
+                continue
+            else:
+                plt.plot(codebook_vals, losses)
+                plt.xlabel('Codebook Values')
+                plt.ylabel('Loss')
+                plt.title('Rate-Distortion')
+                plt.show()
+        else:
+            print("Unknown Function")
+            print("Usage: python3 main.py [function] [model] [epochs]")
+            print("Functions: train, testTrain, showtrain, test, train-rate-distortion, show-rate-distortion")
+    except RuntimeError:
+        print("CUDA Out of Memory. Decreasing Batch Size by Half. New Batch Size: " + str(batch_size/2))
+        batch_size = int(batch_size/2)
+        continue
+    break
